5315eb8934d714a9877ee128aa8e33df784ba33c
Maksym Shcheglov <maxscheglov@gmail.com>
2016-01-21 22:30:30 +0100

    Initial commit.

---
+++ SCrypto/SCrypto.h
@@ -0,0 +1,19 @@
+//
+//  SCrypto.h
+//  SCrypto
+//
+//  Created by Maksym Shcheglov on 21/01/16.
+//  Copyright © 2016 Maksym Shcheglov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SCrypto.
+FOUNDATION_EXPORT double SCryptoVersionNumber;
+
+//! Project version string for SCrypto.
+FOUNDATION_EXPORT const unsigned char SCryptoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SCrypto/PublicHeader.h>
+
+
---
+++ SCrypto/SCrypto.swift
@@ -0,0 +1,13 @@
+//
+//  SCrypto.swift
+//  SCrypto
+//
+//  Created by Maksym Shcheglov on 21/01/16.
+//  Copyright © 2016 Maksym Shcheglov. All rights reserved.
+//
+
+import CommonCrypto
+
+class SCrypto: NSObject {
+
+}
