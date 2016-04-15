800415af823b5b3786a8e00d5c23f45736c45fc8
Maksym Shcheglov <maxscheglov@gmail.com>
2016-04-16 00:35:00 +0100

    Updated the modulemap.sh script to use it from the target runphase as well.

--- modulemap.sh
+++ modulemap.sh
@@ -1,17 +1,23 @@
 #!/bin/sh
 
-BASE_DIR=$(exec pwd)
+if [ "$#" -eq 2 ]; then
+    SDKS=( $1 )
+    BASE_DIR=$2
+else
+	SDKS=( iphoneos iphonesimulator macosx watchsimulator appletvsimulator)
+	BASE_DIR=$(exec pwd)
+fi
+
 echo "BASE_DIR: ${BASE_DIR}"
-SDKS=( iphoneos iphonesimulator macosx watchsimulator appletvsimulator)
 for SDK in "${SDKS[@]}"
 do
-MODULE_DIR="${BASE_DIR}/Frameworks/${SDK}/CommonCrypto.framework"
-SDKPATH=$(eval "xcrun --sdk ${SDK} --show-sdk-path")
-mkdir -p "${MODULE_DIR}"
-echo "module CommonCrypto [system] {\n\
-header \"${SDKPATH}/usr/include/CommonCrypto/CommonCrypto.h\"\n\
-header \"${SDKPATH}/usr/include/CommonCrypto/CommonRandom.h\"\n\
-export *\n\
-}" > "${MODULE_DIR}/module.map"
-echo "Created module map for ${SDK}."
+	MODULE_DIR="${BASE_DIR}/Frameworks/${SDK}/CommonCrypto.framework"
+	SDKPATH=$(eval "xcrun --sdk ${SDK} --show-sdk-path")
+	mkdir -p "${MODULE_DIR}"
+	printf "module CommonCrypto [system] {\n\
+	header \"${SDKPATH}/usr/include/CommonCrypto/CommonCrypto.h\"\n\
+	header \"${SDKPATH}/usr/include/CommonCrypto/CommonRandom.h\"\n\
+	export *\n\
+	}" > "${MODULE_DIR}/module.map"
+	echo "Created module map for ${SDK}."
 done
