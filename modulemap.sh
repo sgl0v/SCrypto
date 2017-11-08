c0d6be81e077ee5a5b5e6fe02e2fa5f2fc245833
Maksym Shcheglov <>
2017-11-08 22:11:31 +0100

    Changed the bash script that takes responsibility of modulemap file generation. It shouldn't depend on the concrete SDK version.

--- modulemap.sh
+++ modulemap.sh
@@ -1,22 +1,27 @@
 #!/bin/sh
 
-if [ "$#" -eq 2 ]; then
-    SDKS=( $1 )
-    BASE_DIR=$2
+if [ "$#" -eq 1 ]; then
+    BASE_DIR=$1
 else
-	SDKS=( iphoneos iphonesimulator macosx watchsimulator appletvsimulator)
 	BASE_DIR=$(exec pwd)
 fi
 
+SDKS=( iphoneos iphonesimulator macosx watchsimulator appletvsimulator)
+SDK_FILE_NAMES=( iPhoneOS.sdk iPhoneSimulator.sdk MacOSX.sdk WatchSimulator.sdk AppleTVSimulator.sdk)
+
 echo "BASE_DIR: ${BASE_DIR}"
-for SDK in "${SDKS[@]}"
-do
+
+for ((i = 0; i < ${#SDKS[@]}; ++i)); do
+	SDK="${SDKS[$i]}"
+	SDK_FILE_NAME="${SDK_FILE_NAMES[$i]}"
+	PLATFORM_PATH=$(eval "xcrun --sdk ${SDK} --show-sdk-platform-path")
+	SDK_PATH="${PLATFORM_PATH}/Developer/SDKs/${SDK_FILE_NAME}"
+
 	MODULE_DIR="${BASE_DIR}/Frameworks/${SDK}/CommonCrypto.framework"
-	SDKPATH=$(eval "xcrun --sdk ${SDK} --show-sdk-path")
-	mkdir -p "${MODULE_DIR}"
+  	mkdir -p "${MODULE_DIR}"
 	printf "module CommonCrypto [system] {\n\
-	header \"${SDKPATH}/usr/include/CommonCrypto/CommonCrypto.h\"\n\
-	header \"${SDKPATH}/usr/include/CommonCrypto/CommonRandom.h\"\n\
+	header \"${SDK_PATH}/usr/include/CommonCrypto/CommonCrypto.h\"\n\
+	header \"${SDK_PATH}/usr/include/CommonCrypto/CommonRandom.h\"\n\
 	export *\n\
 	}" > "${MODULE_DIR}/module.map"
 	echo "Created module map for ${SDK}."
